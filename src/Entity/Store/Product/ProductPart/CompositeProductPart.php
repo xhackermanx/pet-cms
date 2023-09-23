1fc87f94284eee8ffee573335ab7a1eac8b75ef1
xhackermanx <dmitriy.a.gm@gmail.com>
2023-09-23 15:36:11 +0400

    product parts

--- src/Entity/Store/Product/ProductPart/CompositeProductPart.php
+++ src/Entity/Store/Product/ProductPart/CompositeProductPart.php
@@ -2,10 +2,71 @@
 
 namespace App\Entity\Store\Product\ProductPart;
 
+use App\Entity\Store\Product\ProductPart\Field\Field;
 use App\Repository\Store\Product\ProductPart\CompositeProductPartRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
 
 #[ORM\Entity(repositoryClass: CompositeProductPartRepository::class)]
 class CompositeProductPart extends ProductPartComponent
 {
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: ProductPartComponent::class)]
+    private Collection $parts;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->parts = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Field>
+     * @throws Exception
+     */
+    public function getFields(): Collection
+    {
+        $fields = new ArrayCollection();
+
+        foreach ($this->parts as $part) {
+            if (!$part instanceof SimpleProductPart) {
+                throw new Exception('no implementation');
+            }
+            foreach ($part->getFields() as $field) {
+                $fields->add($field);
+            }
+        }
+
+        return $fields;
+    }
+    /**
+     * @return Collection<int, ProductPartComponent>
+     */
+    public function getParts(): Collection
+    {
+        return $this->parts;
+    }
+
+    public function addPart(ProductPartComponent $part): static
+    {
+        if (!$this->parts->contains($part)) {
+            $this->parts->add($part);
+            $part->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removePart(ProductPartComponent $part): static
+    {
+        if ($this->parts->removeElement($part)) {
+            // set the owning side to null (unless already changed)
+            if ($part->getParent() === $this) {
+                $part->setParent(null);
+            }
+        }
+
+        return $this;
+    }
 }
--- src/Entity/Store/Product/ProductPart/ProductPartComponent.php
+++ src/Entity/Store/Product/ProductPart/ProductPartComponent.php
@@ -3,6 +3,7 @@
 namespace App\Entity\Store\Product\ProductPart;
 
 use App\Entity\Store\Product\Product;
+use App\Entity\Store\Product\ProductPart\Field\Field;
 use App\Repository\Store\Product\ProductPart\ProductPartComponentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -28,6 +29,9 @@ abstract class ProductPartComponent
     #[ORM\ManyToMany(targetEntity: Product::class, inversedBy: 'parts')]
     private Collection $product;
 
+    #[ORM\ManyToOne(inversedBy: 'parts')]
+    private ?CompositeProductPart $parent = null;
+
     public function __construct()
     {
         $this->product = new ArrayCollection();
@@ -46,6 +50,11 @@ abstract class ProductPartComponent
         return $this->product;
     }
 
+    /**
+     * @return Collection<int, Field>
+     */
+    abstract public function getFields(): Collection;
+
     public function addProduct(Product $product): static
     {
         if (!$this->product->contains($product)) {
@@ -58,6 +67,18 @@ abstract class ProductPartComponent
     public function removeProduct(Product $product): static
     {
         $this->product->removeElement($product);
+
+        return $this;
+    }
+
+    public function getParent(): ?CompositeProductPart
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?CompositeProductPart $parent): static
+    {
+        $this->parent = $parent;
 
         return $this;
     }
